1958c6925779b5ba5a674f0f7a9f635bda6c51b4
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-08-29 20:52:13 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,18 @@
---https://discord.gg/WrQmADK3
---   _____  _   __       _____                     _           
---  / ___/ / | / /      / ___/ ___   _____ _   __ (_)_____ ___ 
---  \__ \ /  |/ /______ \__ \ / _ \ / ___/| | / // // ___// _ \
--- ___/ // /|  //_____/___/ //  __// /    | |/ // // /__ /  __/
---/____//_/ |_/       /____/ \___//_/     |___//_/ \___/ \___/
-
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 author 'SN-Service'
-description "Aduty System"
-version '2.0.0'
+description "Aduty by DerPrince"
+version '1.2.0'
 
-escrow_ignore {
-    'config.lua',
-}
-  
 client_scripts {
-    'config.lua',
-    'client/client.lua',
+	'client.lua',
+	'config.lua',
 }
-  
+
 server_scripts {
-    'config.lua',
-    'server/server.lua', 
+	'server.lua',
+	'config.lua',
 }
+
+dependency '/assetpacks'
